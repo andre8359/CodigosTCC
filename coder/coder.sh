291ba32908d2679c92cefbfc5307fd6bd2c4b023
Andre Dantas <andre8359@gmail.com>
2015-10-04 01:06:37 -0300

    MELHORANDO ENTRADA DE PARAMENTROS DO CODER

--- coder/coder.sh
+++ coder/coder.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+#variables
+VIDEO=$1
+SIZE="$3x$2"
+NEW_SIZE="$5x$4"
+
+
 #coding
 echo "### CODING ODD FRAMES ###"
-ffmpeg -s 1280x720 -i ../videos/shields.yuv  -filter:v select="mod(n-1\,2)" -vcodec mjpeg -qscale 1  odd.avi
+ffmpeg -s $SIZE -i ../videos/shields.yuv  -filter:v select="mod(n-1\,2)" -vcodec mjpeg -qscale 1  odd.avi
 ffmpeg -i odd.avi -c:v rawvideo -pix_fmt yuv420p odd.yuv
 
 echo "### CODING EVEN FRAMES ###"
-ffmpeg -s 1280x720 -i ../videos/shields.yuv  -filter:v select="not(mod(n-1\,2))" -vcodec mjpeg -qscale 1 -s 640x360 even.avi
+ffmpeg -s $SIZE -i ../videos/shields.yuv   -filter:v select="not(mod(n-1\,2))" -vcodec mjpeg -qscale 1 -s $NEW_SIZE even.avi
 ffmpeg -i even.avi -c:v rawvideo -pix_fmt yuv420p even.yuv
 
 echo "###   DELETING AVI FILES   ###"
