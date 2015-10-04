c23be6c175d643ea4f9fe9137a6a54d7f42322b9
Andre Dantas <andre8359@gmail.com>
2015-10-04 03:07:07 -0300

    DELETANDO ARQUIVOS DESNECESSARIOS

--- decoder/decoder.m
+++ decoder/decoder.m
@@ -1,3 +1,4 @@
 %% decoder: function description
-function [flag,dists] = decoder(even_frames, odd_frames, n_frames_even, n_frames_odd, downsample_factor)
+function [sucess , dists] = decoder(even_frames, odd_frames,  downsample_factor)
+
 
