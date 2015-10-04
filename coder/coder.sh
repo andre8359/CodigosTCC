336f449dc4ca0ee69b85bc3a3b772e433b05e735
Andre Dantas <andre8359@gmail.com>
2015-10-03 23:12:57 -0300

    TROCANDO OUTPUTS DO CODER PARA YUV

--- coder/coder.sh
+++ coder/coder.sh
@@ -2,6 +2,12 @@
 
 #coding
 echo "### CODING ODD FRAMES ###"
-ffmpeg -s 1280x720 -i 720p50_shields_ter.yuv  -filter:v select="mod(n-1\,2)" -vcodec mjpeg -qscale 1  odd.avi
+ffmpeg -s 1280x720 -i ../videos/shields.yuv  -filter:v select="mod(n-1\,2)" -vcodec mjpeg -qscale 1  odd.avi
+ffmpeg -i odd.avi -c:v rawvideo -pix_fmt yuv420p odd.yuv
+
 echo "### CODING EVEN FRAMES ###"
-ffmpeg -s 1280x720 -i 720p50_shields_ter.yuv  -filter:v select="not(mod(n-1\,2))" -vcodec mjpeg -qscale 1 -s 640x360 even.avi
+ffmpeg -s 1280x720 -i ../videos/shields.yuv  -filter:v select="not(mod(n-1\,2))" -vcodec mjpeg -qscale 1 -s 640x360 even.avi
+ffmpeg -i even.avi -c:v rawvideo -pix_fmt yuv420p even.yuv
+
+echo "###   DELETING AVI FILES   ###"
+rm -irf *.avi
