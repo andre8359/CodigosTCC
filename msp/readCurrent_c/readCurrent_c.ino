a50d1fe068af17c849a4d84d1ae40f04c9f4b1a5
Andre Dantas <andre8359@gmail.com>
2015-10-18 19:16:19 -0200

    ADICIONANDO EXTENSOES NO GITIGNORE

--- msp/readCurrent_c/readCurrent_c.ino
+++ msp/readCurrent_c/readCurrent_c.ino
@@ -1,4 +1,7 @@
-long int t = 0; 
+int sensorValue;
+float resistor = 10.03*1000;
+float voltage = 0; 
+float current = 0;
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600); // msp430g2231 must use 4800
@@ -9,11 +12,11 @@ void setup() {
 // the loop routine runs over and over again forever:
 void loop() {
   // read the input pin:
-  t++;
+  sensorValue = analogRead(A3);
   // print out the state of the button:
-  Serial.println(buttonState);
-  if( t == 1000 )  t = 0;
-  delay(100);        // delay in between reads for stability
+  voltage = sensorValue * (3.0 / 1023.0);
+  
+  Serial.println(voltage);
 }
 
 
