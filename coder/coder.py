a8a7cfab4c22e07277fa532056ede1587b1760c6
Andre Dantas <andre8359@gmail.com>
2015-10-19 13:47:16 -0200

    IMPLEMENTANDO MELHORIAS NO CODIFICADOR (CALCULO DE BITRATE)

--- coder/coder.py
+++ coder/coder.py
@@ -7,15 +7,17 @@ class coder():
         self.video  = video
         self.H = H
         self.W = W
+        self.downsampleFactor = downsampleFactor
         self.newH = int(float(H*downsampleFactor))
         self.newW = int(float(W*downsampleFactor))
         self.qscale = 1
         self.dir_ = dir_
+        self.video_odd = video + "_odd.yuv"
+        self.video_even = video + "_even.yuv"
 
-    def __DeleteAviFiles__(self):
-        os.system("rm -irf *.avi")
 
-    def __coderJPEG__(self):
+
+    def  coderJPEG(self):
         ffmpeg_ = "ffmpeg "
         rate_ = "-r 2 -s "
         filter_ = " -filter:v select=\"mod(n-1\,2)\" "
@@ -35,15 +37,38 @@ class coder():
         os.system(ffmpeg_ + " -i " +outputFIle_ + ".avi"+ " -c:v rawvideo -pix_fmt yuv420p "+outputFIle_ + ".yuv")
 
         if returnOdd + returnEven > 0:
-            print "### HEY MOTHER FUCKER ! SOMETHINK ARE WRONG, LOOK UP!!!! ###"
+            print("### ERRO!!!! ###")
             quit()
 
-        self.__DeleteAviFiles__()
+        self.deleteAviFiles()
 
 
-    def __coderH264__(self):
+    def coderH264(self):
         print("TRETA")
 
-    def __CalcBitRate__(self):
-        print("TRETA")
+    def calcBitRate(self,):
 
+        fileR = open("bitrates.txt","a")
+
+        if  fileR == 0 :
+            print("### NAO FOI POSSIVEL ABRIR bitrates.txt !!! ###")
+            quit()
+
+        odd = os.stat(self.video_odd)
+        even = os.stat(self.video_even)
+
+        length_odd = float(odd.st_size)
+        length_even = float(even.st_size)
+
+        bitrate_odd = length_odd / (self.H*self.W) *1.0
+        bitrate_even = length_even / (self.H*self.W) *1.0
+        fileR.write(self.video + "  " + self.downsampleFactor.__str__() + \
+                                                "  " + bitrate_odd.__str__() + "    " + bitrate_even.__str__() + "\n")
+
+        fileR.close()
+
+
+    def deleteAviFiles(self):
+        os.system("rm -irf *.avi")
+    def deleteVideoFiles(self):
+        os.system("rm -irf *.avi *.yuv")
--- coder/main.py
+++ coder/main.py
@@ -3,4 +3,6 @@
 from coder import *
 
 cod = coder("foreman", 288, 352, 1.0/8,"../videos/" )
-cod.__coderJPEG__()
+cod.coderJPEG()
+cod.calcBitRate()
+cod.deleteVideoFiles()
