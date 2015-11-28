7ae47865dbcdb3553570deb7cc642d02067acc10
Andre Dantas <andre8359@gmail.com>
2015-11-28 14:01:43 -0200

    TESTES CONSUMO H264

--- coder/codificacao_resolucao_mista.py
+++ coder/codificacao_resolucao_mista.py
@@ -2,6 +2,17 @@ from coder import *
 import sys
 import os
 
+# video = sys.argv[1]
+# H = int(sys.argv[2])
+# W = int(sys.argv[3])
+# dir_ = sys.argv[4]
+# qscale = int(sys.argv[5])
+# downsampleFactor = float(sys.argv[6])
+
+# cod = coder(video,H,W,downsampleFactor,dir_,1,qscale)
+# cod.codingJPEGOddFrames()
+# cod.coderJPEG()
+
 video = sys.argv[1]
 H = int(sys.argv[2])
 W = int(sys.argv[3])
@@ -10,6 +21,5 @@ qscale = int(sys.argv[5])
 downsampleFactor = float(sys.argv[6])
 
 cod = coder(video,H,W,downsampleFactor,dir_,1,qscale)
-cod.codingJPEGOddFrames()
-cod.coderJPEG()
-
+cod.codingH264OddFrames()
+cod.coderH264()
--- coder/codificacao_resolucao_original.py
+++ coder/codificacao_resolucao_original.py
@@ -2,6 +2,20 @@ from coder import *
 import sys
 import os
 
+# video = sys.argv[1]
+# H = int(sys.argv[2])
+# W = int(sys.argv[3])
+# dir_ = sys.argv[4]
+# qscale = int(sys.argv[5])
+# downsampleFactor = float(sys.argv[6])
+
+# cod = coder(video,H,W,downsampleFactor,dir_,1,qscale)
+
+# cod.codingJPEGOddFrames()
+# cod.codingJPEGOriginalEvenFrames()
+
+
+
 video = sys.argv[1]
 H = int(sys.argv[2])
 W = int(sys.argv[3])
@@ -11,5 +25,6 @@ downsampleFactor = float(sys.argv[6])
 
 cod = coder(video,H,W,downsampleFactor,dir_,1,qscale)
 
-cod.codingJPEGOddFrames()
-cod.codingJPEGOriginalEvenFrames()
+cod.codingH264OddFrames()
+cod.codingh264riginalEvenFrames()
+
